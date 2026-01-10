66228d057fff1799874e26a1bd3b8263bf454c1d
Migres575 <miquelgregori@gmail.com>
2026-01-10 22:34:22 +0100

    Refactor Vite config for SWC and server settings
    Updated Vite configuration to use @vitejs/react-swc and added server settings.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite";
+import react from "@vitejs/react-swc";
+import path from "path";
 
 export default defineConfig({
-  base: "/valenci-pau-connect/",
+  base: "/valenci-pau-connect/", // AQUESTA LÍNIA ÉS LA CLAU
+  server: {
+    host: "0.0.0.0",
+    port: 8080,
+  },
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
