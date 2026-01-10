25f59252119c11bf2fe261801fa4244578849aef
Migres575 <miquelgregori@gmail.com>
2026-01-10 23:42:43 +0100

    Improve root element rendering in main.tsx
    Refactor root element rendering with error handling.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Aquest fitxer connecta el codi de React amb el "root" del teu index.html
+const rootElement = document.getElementById("root");
+
+if (!rootElement) throw new Error("No s'ha trobat l'element root al document.");
+
+createRoot(rootElement).render(<App />);
